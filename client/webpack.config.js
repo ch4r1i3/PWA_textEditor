9c42944bac18d5ccc7024583ac0246912f727dde
ch4r1i3 <111701536+ch4r1i3@users.noreply.github.com>
2023-02-26 11:33:00 -0500

    finishing the js from the client side and webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -7,24 +7,75 @@ const { InjectManifest } = require('workbox-webpack-plugin');
 // TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
-  return {
-    mode: 'development',
-    entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
-    },
-    output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
-    },
-    plugins: [
-      
-    ],
+	return {
+		mode: "development",
+		// Entry point for files.
+		entry: {
+			main: "./src/js/index.js",
+			install: "./src/js/install.js",
+		},
+		// Output for our bundles.
+		output: {
+			filename: "[name].bundle.js",
+			path: path.resolve(__dirname, "dist"),
+		},
+		plugins: [
+			// Webpack plugin that generates our html file and injects our bundles.
+			new HtmlWebpackPlugin({
+				template: "./index.html",
+				title: "Just Another Text Editor",
+			}),
 
-    module: {
-      rules: [
-        
-      ],
-    },
-  };
+			// Injects our custom service worker.
+			new InjectManifest({
+				swSrc: "./src-sw.js",
+				swDest: "src-sw.js",
+			}),
+
+			// Creates a manifest.json file.
+			new WebpackPwaManifest({
+				fingerprints: false,
+				inject: true,
+				name: "Just Another Text Editor",
+				short_name: "J.A.T.E.",
+				description: "Create notes with or without an internet connection!",
+				background_color: "#272822",
+				theme_color: "#272822",
+				start_url: "./",
+				publicPath: "./",
+				icons: [
+					{
+						src: path.resolve("src/images/logo.png"),
+						sizes: [96, 128, 192, 256, 384, 512],
+						destination: path.join("assets", "icons"),
+					},
+				],
+			}),
+		],
+
+		module: {
+			rules: [
+				// Add CSS loaders to webpack.
+				{
+					test: /\.css$/i,
+					use: ["style-loader", "css-loader"],
+				},
+				// Add babel to webpack in order to use ES6.
+				{
+					test: /\.m?js$/,
+					exclude: /node_modules/,
+					use: {
+						loader: "babel-loader",
+						options: {
+							presets: ["@babel/preset-env"],
+							plugins: [
+								"@babel/plugin-proposal-object-rest-spread",
+								"@babel/transform-runtime",
+							],
+						},
+					},
+				},
+			],
+		},
+	};
 };
