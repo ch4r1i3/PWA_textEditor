2ad7e79c36fb43e00ef8c6767641dc379d9147aa
ch4r1i3 <111701536+ch4r1i3@users.noreply.github.com>
2023-02-26 12:07:25 -0500

    add StaleWhileRevalidate

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,6 +1,6 @@
 const { warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst } = require('workbox-strategies');
-const { registerRoute } = require('workbox-routing');
+const { CacheFirst, StaleWhileRevalidate } = require("workbox-strategies");
+const { registerRoute } = require("workbox-routing");
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
 const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
